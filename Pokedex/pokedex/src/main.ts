0ee746d54b9b6aad0c3b68ceca7f64780aba0ff5
Gabriel Betin Valdes <gabrielbetinvaldes@gmail.com>
2023-06-22 19:26:01 -0500

    Commits#13

--- Pokedex/pokedex/src/main.ts
+++ Pokedex/pokedex/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,15 @@ async function bootstrap() {
   // De esta manera podemos colocar un prefijo para las api
   // Se consumiria asi http://localhost:3000/api/v2/pokemon
   app.setGlobalPrefix('api/v2');
+
+
+  // Para trabajar con los pipes de class validatir
+  app.useGlobalPipes(
+    new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    })
+    );
 
 
   await app.listen(3000);
--- Pokedex/pokedex/src/pokemon/entities/pokemon.entity.ts
+++ Pokedex/pokedex/src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,25 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+// Extiende de Document de monggose
+// Se debe colocar el decorador de squema de mongo
+@Schema()
+export class Pokemon  extends Document{
+
+    // El ID no se especifica porque
+    // Mongo lo da
+    @Prop({
+        unique:true,
+        index: true
+
+    })	
+    name:string;
+
+    @Prop({
+        unique:true,
+        index: true
+
+    })
+    no: number;
+}
+export const PokemonSchema = SchemaFactory.createForClass( Pokemon);
