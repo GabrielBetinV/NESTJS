af71d85b7c29eccca0c56b9ea765719c190bfd0a
Gabriel Betin Valdes <gabrielbetinvaldes@gmail.com>
2023-07-03 15:24:28 -0500

    Commits #21

---
+++ PostgresSQL/teslo-shop/src/products/dto/create-product.dto.ts
@@ -0,0 +1,45 @@
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, 
+    IsPositive, IsString, MinLength 
+} from 'class-validator';
+
+
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()
+    @IsOptional()
+    slug?: string;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock?: number; 
+
+    // Indicamos que es un arreglo de string
+    @IsString({ each: true })
+    @IsArray()
+    sizes: string[]
+
+    // Que este dentro de estos valores configurados
+    @IsIn(['men','women','kid','unisex'])
+    gender: string;
+
+    // @IsString({ each: true })
+    // @IsArray()
+    // @IsOptional()
+    // tags: string[];
+
+
+}
